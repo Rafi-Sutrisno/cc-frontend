2db270c519f393171b426a5b479652913870c937
Rafi Sutrisno <mrafis2003@gmail.com>
2024-05-25 10:10:30 +0700

    feat: fix app jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import React from "react";
 import GetuserObject from "./assets/Get-user/button-get-user";
+import TokenObject from "./assets/Token/showToken";
+import RegisterObject from "./assets/Register/Register";
 import "./App.css";
 
 function App() {
   return (
     <>
-      <h1>Hello World!</h1>
-      <GetuserObject />
+      <div className="flex">
+        <h1>Hello World!</h1>
+        <RegisterObject />
+        <TokenObject />
+        <GetuserObject />
+      </div>
     </>
   );
 }
