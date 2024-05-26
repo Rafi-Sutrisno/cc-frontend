2dad88450c56338cc2ca500df0a613372468b870
Rafi Sutrisno <mrafis2003@gmail.com>
2024-05-26 13:52:01 +0700

    feat: add routes

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,32 @@ import GetuserObject from "./assets/get-user-new/getUser";
 import TokenObject from "./assets/Token/showToken";
 import RegisterObject from "./assets/Register/Register";
 import LoginObject from "./assets/Login/Login-test";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./App.css";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <h1>Hello World!</h1>,
+  },
+  {
+    path: "/register",
+    element: <RegisterObject />,
+  },
+  {
+    path: "/login",
+    element: <LoginObject />,
+  },
+  {
+    path: "/getuser",
+    element: <GetuserObject />,
+  },
+]);
 
 function App() {
   return (
     <>
-      <div className="flex">
-        <h1>Hello World!</h1>
-        <LoginObject />
-        <RegisterObject />
-        <TokenObject />
-        <GetuserObject />
-      </div>
+      <RouterProvider router={router} />
     </>
   );
 }
