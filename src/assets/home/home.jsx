90afb1ec2d6f9d4d8da85d198bde4ae80661e04f
Rafi Sutrisno <mrafis2003@gmail.com>
2024-05-28 12:38:40 +0700

    feat: add home page

---
+++ src/assets/home/home.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import "./home.css";
+import { useNavigate } from "react-router-dom";
+
+const registerUser = () => {
+  const navigate = useNavigate();
+
+  return (
+    <>
+      <div>
+        <h1>Hello World!</h1>
+        <div className="home">
+          <button onClick={() => navigate("/register")} className="button">
+            Go to Register
+          </button>
+          <button onClick={() => navigate("/login")} className="button">
+            Go to Login
+          </button>
+          <button onClick={() => navigate("/getuser")} className="button">
+            Go to Get User
+          </button>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default registerUser;
